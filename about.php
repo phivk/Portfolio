5f51df312455e158cd66126cdd402f4a8caa955c
Philo van Kemenade <phillchill@gmail.com>
2012-12-09 22:09:16 +0000

    about text edit

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
       <section class="light link_underline link_hover_red textBackground">
         <p>Hi, my name is Philo. I'm currently living in London, where I graduated from the MSc Cognitive Computing at Goldsmiths College. I'm originally from Amsterdam, where I studied Artificial Intellingence and got into <a href="http://vimeo.com/phillchill" target="_blank">filmmaking</a>.</p> 
         <p>I came to London to merge my expertise in AI with my passion for video and work on ways to use online interaction data to <a href="/projects/human_computation.php" target="_blank">filter and reconfigure</a> large collections of user-generated video.</p>
-        <p>I am interested in innovative ways of telling stories online through video, images, text and sound and like to play with <a href="http://popcornjs.org/" target="_blank">technologies</a> that enable new opportunities. I like to think about how technology can support learning and teaching and like to <a href="http://picniclondon.tumblr.com" target="_blank">share my skills</a> with people around me.</p>
+        <p>I am interested in innovative ways of telling stories through video, images, text and sound and like to play with <a href="http://popcornjs.org/" target="_blank">technologies</a> that enable new opportunities. I like to think about how technology can support learning and teaching and like to <a href="http://picniclondon.tumblr.com" target="_blank">share my skills</a> with people around me.</p>
         <p>I love cycling and walking on my hands.</p>
         
       </section>
